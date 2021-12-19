5eba432e7b139ed0aeab9a394a6ea015d7084816
Eli Vance <wagren@kth.se>
2021-12-19 13:36:25 +0100

    rename header file to colors, since there are several colors and not just one xd

---
+++ src/colors.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+const uint8_t WHITE[3]  = { 255, 255, 255};
+const uint8_t BLACK[3]  = {   0,   0,   0};
+const uint8_t GRAY[3]   = {  40,  40,  40};
+const uint8_t RED[3]    = { 255,   0,   0};
+const uint8_t GREEN[3]  = {   0, 255,   0};
+const uint8_t BLUE[3]   = {   0,   0, 255};
+
+#endif
