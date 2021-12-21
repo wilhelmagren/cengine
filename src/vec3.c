54fd511b975a11c02e03b57e35ccc85fc6315967
Eli Vance <wagren@kth.se>
2021-12-21 21:01:09 +0100

    rename operation functions

--- src/vec3.c
+++ src/vec3.c
@@ -8,7 +8,7 @@ Vec3* Vec3_Constructor(f8 x, f8 y, f8 z) {
     vec->x = x;
     vec->y = y;
     vec->z = z;
-    vec->magnitude = _Magnitude(x, y, z);
+    vec->magnitude = _Vec3Magnitude(x, y, z);
     vec->pointers = 0;
     return vec;
 }
@@ -17,29 +17,29 @@ Vec3* _CopyVec3(Vec3* u) {
     return Vec3_Constructor(u->x, u->y, u->z);
 }
 
-void _Add(Vec3* u, Vec3* v, Vec3* res) {
+void _Vec3Add(Vec3* u, Vec3* v, Vec3* res) {
     res->x = u->x + v->x;
     res->y = u->y + v->y;
     res->z = u->z + v->z;
 }
 
-void _Subtract(Vec3* u, Vec3* v, Vec3* res) {
+void _Vec3Subtract(Vec3* u, Vec3* v, Vec3* res) {
     res->x = u->x - v->x;
     res->y = u->y - v->y;
     res->z = u->z - v->z;
 }
 
-void _CrossProduct(Vec3* u, Vec3* v, Vec3* res) {
+void _Vec3CrossProduct(Vec3* u, Vec3* v, Vec3* res) {
     res->x = u->y*v->z - u->z*v->y;
     res->y = u->z*v->x - u->x*v->z;
     res->z = u->x*v->y - u->y*v->x;
 }
 
-f8 _DotProduct(Vec3* u, Vec3* v) {
+f8 _Vec3DotProduct(Vec3* u, Vec3* v) {
     return u->x*v->x + u->y*v->y + u->z*v->z;
 }
 
-f8 _Magnitude(f8 x, f8 y, f8 z) {
+f8 _Vec3Magnitude(f8 x, f8 y, f8 z) {
     return (f8)sqrt(x*x + y*y + z*z);
 }
 
--- src/vec3.h
+++ src/vec3.h
@@ -10,10 +10,10 @@ typedef struct {
 
 Vec3* Vec3_Constructor(f8 x, f8 y, f8 z);
 Vec3* _CopyVec3(Vec3* u);
-void _Add(Vec3* u, Vec3* v, Vec3* res);
-void _Subtract(Vec3* u, Vec3* v, Vec3* res);
-void _CrossProduct(Vec3* u, Vec3* v, Vec3* res);
-f8 _DotProduct(Vec3* u, Vec3* v);
-f8 _Magnitude(f8 x, f8 y, f8 z);
+void _Vec3Add(Vec3* u, Vec3* v, Vec3* res);
+void _Vec3Subtract(Vec3* u, Vec3* v, Vec3* res);
+void _Vec3CrossProduct(Vec3* u, Vec3* v, Vec3* res);
+f8 _Vec3DotProduct(Vec3* u, Vec3* v);
+f8 _Vec3Magnitude(f8 x, f8 y, f8 z);
 
 #endif
